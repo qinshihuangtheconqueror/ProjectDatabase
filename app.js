b848b05830c8b2437f7dd9969ea2024fb3ceda1a
Lê Hoàng Huy <113824447+qinshihuangtheconqueror@users.noreply.github.com>
2023-12-06 19:37:09 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require ('express');
+var session = require ('express-session');
+var cookie = require ('cookie-parser');
+var path = require ('path');
+var ejs= require ('ejs');
+var multer = require('multer');
+var path = require ('path');
+var async = require ('async');
+var nodmailer = require ('nodemailer');
+var crypto = require ('crypto');
+var expressValidator = require ('express-validator');
+var  sweetalert = require('sweetalert2');
+var app = express();
+
+var bodyParser = require ('body-parser')
+
