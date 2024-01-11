2047dd36caded582cb565500229abfda5a6cddea
Viet Anh <vanhtran18.soict@gmail.com>
2024-01-11 23:51:11 +0700

    Dynamic doctors

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,15 @@ module.exports = app => {
     const authMiddleware = require('../middlewares/auth.middleware.admin')
 
     router.get('/adminDoctors', authMiddleware.loggedin, async(req, res)=>{
-        sql.query(config, 'SELECT * FROM staff', (err, results) => {
+        sql.query(config, 'SELECT * FROM staff JOIN specialization ON staff.specialization_id = specialization.specialization_id', (err, results) => {
           const doctors = results;
+          doctors.forEach(doctor => {
+            router.get(`/adminDoctors/${doctor.Staff_ID}`, authMiddleware.loggedin, (req, res) => {
+              const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+              const formattedDate = new Date(doctor.DOB).toLocaleDateString('en-GB', options);
+              res.render('adminDoctorIn4', { doctor, formattedDate });
+            });
+          });
           sql.query(config, 'SELECT COUNT(staff_id) AS staffCount FROM staff', (err, countResult) => {
             const staffCount = countResult[0].staffCount;
             sql.query(config, 'SELECT COUNT(patient_id) AS patientCount FROM Patient', (err, countRes) =>{
@@ -22,7 +29,6 @@ module.exports = app => {
         const { Name, Phone, Specialization, Salary, Address, DOB, Gender } = req.body;
         sql.query(config, 'SELECT Specialization_ID FROM Specialization WHERE Specialization_Name = ?', [Specialization], (err, results) => {
           const Specialization_ID = results[0].Specialization_ID;
-          console.log(req.body)
           if (Name && Phone){
             sql.query(config, 'INSERT INTO staff (Name, Salary, Phone, Address, DOB, Specialization_ID, Gender) VALUES (?, ?, ?, ?, ?, ?, ?)', [Name, Salary, Phone, Address, DOB, Specialization_ID, Gender], (err, result) => {
               if (err) {
@@ -36,10 +42,10 @@ module.exports = app => {
           }
         });        
       });
-
-    router.delete('/adminDoctors', async (req, res) => {
-        const { Staff_Id } = req.body;
       
+      router.delete('/adminDoctors', async (req, res) => {
+        const { Staff_Id } = req.body;
+        
         // Check if the Staff_Id for removing the doctor is present
         if (Staff_Id) {
           // Remove the doctor from the database using Staff_Id
@@ -58,6 +64,15 @@ module.exports = app => {
         }
       });
 
+    router.post('/adminDoctors/in4', authMiddleware.loggedin, (req, res) => {
+      const { doctorId } = req.body;
+      let doctor;
+      sql.query(config, 'SELECT * FROM staff WHERE Staff_ID = ?', [doctorId] ,(err, result) => {
+        doctor = result;
+      });
+      res.render('adminDoctorIn4', { doctor });
+  });  
+      
     router.get('/adminAnalyst', authMiddleware.loggedin, (req,res)=>{
       sql.query(config, 'SELECT * FROM staff', (err, results) => {
         const doctors = results;
@@ -69,10 +84,6 @@ module.exports = app => {
           })
         });
       });  
-    });
-    
-    router.get('/adminDoctors/in4', authMiddleware.loggedin, (req,res)=>{
-            res.render('adminDoctorIn4');
     });
 
     router.get('/adminAppointment', authMiddleware.loggedin, (req,res)=>{
